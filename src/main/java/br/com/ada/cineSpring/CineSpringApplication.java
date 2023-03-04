e905dc77f05a2353eed7dd8c7b2fbdf534b1aad2
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-03-03 21:48:21 -0300

    Initial project configuration

---
+++ src/main/java/br/com/ada/cineSpring/CineSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.cineSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineSpringApplication.class, args);
+	}
+
+}
